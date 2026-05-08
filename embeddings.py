e7cf6a51f7aa2db5bb412a85ff91014dc684bf83
AntoniKingston <qqtrol@gmail.com>
2026-05-08 13:28:38 +0200

    Embeddings

---
+++ embeddings.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn as nn
+import torch.nn.parameter as parameter
+
+
+
+class Embedding(nn.Module):
+    def __init__(self, num_embedding, embedding_dim):
+        self.weight = parameter.Parameter(torch.Tensor(num_embedding, embedding_dim))
+    def forward(self, x):
+        return self.weight[x, :]
+
+class SinusoidalPositionalEmbedding(nn.Module):
+    def __init__(self, block_size, embedding_dim):
+        self.table = torch.Tensor([[torch.sin(pos / 10000 ** (2 * i / embedding_dim)) if (i % 2 == 0) else torch.cos(pos / 10000 ** (2 * i / embedding_dim)) for i in range(embedding_dim)] for pos in range(block_size)])
+    def forward(self, x):
+        _, T = x.shape
+        # Positional encoding is constant across in inputs, it's length just has to mach input's.
+        return self.table[:T, :]
--- tokenizer.py
+++ tokenizer.py
@@ -1,2 +1,45 @@
-def bpe_tokenizer(text, num_merges):
-    pass
+import re
+from collections import Counter
+from typing import Callable, List, Tuple
+
+Merge = Tuple[Tuple[str, str], str]
+def _most_common_consecutive_pair(tokens: List[str]) -> Merge:
+    counter = Counter(zip(tokens[:-1], tokens[1:]))
+    most_common, _ = counter.most_common(1)[0]
+    return most_common, most_common[0] + most_common[1]
+
+def _apply_merge(tokens: List[str], merge: Merge) -> List[str]:
+    i = 0
+    while i < len(tokens):
+        if i < len(tokens) - 1 and "".join(tokens[i:i+2]) == merge[1]:
+            tokens[i:i+2] = [merge[1]]
+        # Advance always by 1 since if merge is applied the list gets shortened by 1
+        i+=1
+    return tokens
+def bpe_tokenizer(text: str, num_merges: int, info_interval=0) -> Tuple[Callable[[str], List[int]], Callable[[List[int]], str], List[str], List[Merge]]:
+    tokens = list(text)
+    merges : List[Merge] = []
+
+    for i in range(num_merges):
+        if info_interval and i % info_interval == 0:
+            print(f"Learnt {i} merges out of {num_merges}")
+        merge = _most_common_consecutive_pair(tokens)
+        merges.append(merge)
+        tokens = _apply_merge(tokens, merge)
+
+    # Both initial tokens and those learnt by merges are included in vocab
+    vocab = sorted(list(set(tokens) | set(list(text))))
+    stoi = {token: i for i, token in enumerate(vocab)}
+    itos = {i: token for i, token in enumerate(vocab)}
+
+    def encode(text: str) -> List[int]:
+        tokens = list(text)
+        for merge in merges:
+            tokens = _apply_merge(tokens, merge)
+        return [stoi[token] for token in tokens]
+
+    def decode(tokens: List[int]) -> str:
+        return "".join([itos[token] for token in tokens])
+
+    return encode, decode, vocab, merges
+
